48796e70f40022bbc29fe8808c97415a30d89712
Daulet <80630210+amandosd@users.noreply.github.com>
2022-07-09 00:42:25 +0600

    implement flat with stack pop push destructuring

--- implement-flat/index.js
+++ implement-flat/index.js
@@ -12,10 +12,44 @@ function flat(arr, depth = 1) {
   return newArr;
 }
 
+function flat2(arr) {
+  const stack = [...arr];
+  const res = [];
+
+  while (stack.length) {
+    let next = stack.pop();
+
+    if (Array.isArray(next)) {
+      stack.push(...next);
+    } else {
+      res.push(next);
+    }
+  }
+
+  return res.reverse();
+}
+
+function flat3(arr, depth = 1) {
+  const stack = arr.map((item) => [item, depth]);
+  let res = [];
+
+  while (stack.length) {
+    const [item, itemDepth] = stack.pop();
+
+    if (Array.isArray(item) && itemDepth > 0) {
+      stack.push(...item.map((item) => [item, itemDepth - 1]));
+    } else {
+      res.push(item);
+    }
+  }
+
+  return res.reverse();
+}
+
 export const implementFlat = () => {
   const arr = [1, [2], [3, [4]]];
 
-  flat(arr);
+  console.log(flat3(arr, 1));
 
   // console.log(flat(arr));
   // // [1, 2, 3, [4]]
